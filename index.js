8193a6bb219ec72a221d8a15be2c98f671194327
Molotow <drumber13@gmail.com>
2020-02-04 20:04:30 +0200

    move from base.js to index.js some code

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
