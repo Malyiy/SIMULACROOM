218185c42e2408f2316c9e50e508be91d8fc31b6
Molotow <drumber13@gmail.com>
2020-02-05 09:37:07 +0200

    settings for firebase

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
