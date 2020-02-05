1e660c1e7a497325259815f6b68e72b1b79ecca7
Molotow <drumber13@gmail.com>
2020-02-05 10:34:08 +0200

    firebaseUI

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,14 @@
 var firebase = require("firebase/app");
 require("firebase/auth");
 require("firebase/firestore");
+var firebaseui = require('firebaseui');
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+});
+
 
